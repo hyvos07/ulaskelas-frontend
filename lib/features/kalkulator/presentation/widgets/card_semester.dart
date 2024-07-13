e16a3ace76fd40f7faa19a6155fc6282a3d1b546
Hyvos <daniel.liman07@gmail.com>
2024-07-13 23:30:30 +0700

    add: Changing the design of the Semester Cards

--- lib/features/kalkulator/presentation/widgets/card_semester.dart
+++ lib/features/kalkulator/presentation/widgets/card_semester.dart
@@ -14,6 +14,7 @@ class CardSemester extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
+        width: double.infinity,
         padding: const EdgeInsets.symmetric(
           horizontal: 17.5,
           vertical: 12,
@@ -23,37 +24,30 @@ class CardSemester extends StatelessWidget {
           boxShadow: BoxShadowDecorator().defaultShadow(context),
           borderRadius: BorderRadius.circular(8),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              'Semester ${model.givenSemester}',
-              style: FontTheme.poppins14w700black().copyWith(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const HeightSpace(2),
-            Text(
-              model.semesterGPA?.toStringAsFixed(2) ?? '0.00',
-              style: FontTheme.poppins12w400black(),
-            ),
-            const HeightSpace(1),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 1),
-                  decoration: BoxDecoration(
-                    color: BaseColors.gray3.withOpacity(0.6),
-                    borderRadius: BorderRadius.circular(3),
+                Text(
+                  'Semester ${model.givenSemester}',
+                  style: FontTheme.poppins14w700black().copyWith(
+                    fontWeight: FontWeight.w600,
                   ),
-                  child: const SizedBox(
-                    width: 50,
-                    height: 3,
-                  ),
-                )
+                ),
+                const HeightSpace(2),
+                Text(
+                  model.semesterGPA?.toStringAsFixed(2) ?? '0.00',
+                  style: FontTheme.poppins12w400black(),
+                ),
               ],
-            )
+            ),
+            const Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: 11,
+              color: BaseColors.gray1,
+            ),
           ],
         ),
       ),
