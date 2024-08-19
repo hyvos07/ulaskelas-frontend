7b8c7613b727308d052bab85f0a99601e43083b3
Hyvos <daniel.liman07@gmail.com>
2024-08-19 20:25:08 +0700

    fix: Re-styling Bottom NavBar

--- lib/src/bar/ristek_bottom_navigation_bar.dart
+++ lib/src/bar/ristek_bottom_navigation_bar.dart
@@ -8,7 +8,9 @@ part of '_bar.dart';
 class NewRistekBotNavBar extends StatefulWidget {
   /// Creates A Ristek Bottom Navigation Bar.
   const NewRistekBotNavBar({
-    required this.onTap, required this.items, super.key,
+    required this.onTap,
+    required this.items,
+    super.key,
     this.initialActiveIndex = 0,
   });
 
@@ -23,19 +25,21 @@ class NewRistekBotNavBar extends StatefulWidget {
 class _NewRistekBotNavBarState extends State<NewRistekBotNavBar> {
   @override
   Widget build(BuildContext context) {
+    final iconSize = 24.0;
     final theme = Theme.of(context);
     return Container(
-      padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+      padding: const EdgeInsets.fromLTRB(8, 15, 8, 0),
       decoration: BoxDecoration(
         color: theme.colorScheme.background,
         boxShadow: BoxShadowDecorator().defaultShadow(context),
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: widget.items.map((e) {
           final isSelected =
               widget.items.indexOf(e) == widget.initialActiveIndex;
-          return Expanded(
+          return SizedBox(
+            width: e.text.toString() == 'Tanya Teman' ? 85 : 60,
             child: InkWell(
               onTap: () => widget.onTap(widget.items.indexOf(e)),
               child: Column(
@@ -44,21 +48,26 @@ class _NewRistekBotNavBarState extends State<NewRistekBotNavBar> {
                   if (e.icon != null)
                     Icon(
                       e.icon,
-                      color: isSelected 
-                        ? theme.colorScheme.primary : theme.disabledColor,
-                      size: 24,
+                      color: isSelected
+                          ? theme.colorScheme.primary
+                          : theme.disabledColor,
+                      size: iconSize,
                     )
                   else if (e.svgIcon != null)
                     SvgPicture.asset(
                       e.svgIcon!,
-                      color: isSelected 
-                        ? theme.colorScheme.primary : theme.disabledColor,
-                      width: 24,
-                      height: 24,
+                      color: isSelected
+                          ? theme.colorScheme.primary
+                          : theme.disabledColor,
+                      width: iconSize,
+                      height: iconSize,
                     ),
                   const HeightSpace(5),
-                  Text(e.text.toString(),
+                  Text(
+                    e.text.toString(),
                     textAlign: TextAlign.center,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
                     style: theme.textTheme.caption?.copyWith(
                       fontWeight:
                           isSelected ? FontWeight.w500 : FontWeight.w400,
@@ -67,7 +76,7 @@ class _NewRistekBotNavBarState extends State<NewRistekBotNavBar> {
                           : theme.disabledColor,
                     ),
                   ),
-                  const HeightSpace(5),
+                  const HeightSpace(7),
                   AnimatedContainer(
                     duration: const Duration(milliseconds: 450),
                     curve: Curves.easeInQuad,
