7fce832c939660b976c9e0d9fdd1f15f22c73f8b
Fideligo <mrafighalibf@gmail.com>
2025-12-01 15:03:18 +0700

    chore(home): register new ad widgets in export file

--- lib/features/home/presentation/widgets/_widgets.dart
+++ lib/features/home/presentation/widgets/_widgets.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ristek_material_component/ristek_material_component.dart';
@@ -5,6 +6,10 @@ import 'package:ulaskelas/core/theme/_theme.dart';
 import 'package:ulaskelas/features/home/data/models/_models.dart';
 import 'package:ulaskelas/features/matkul/form/data/models/_models.dart';
 
+part 'ads_carousel.dart';
+part 'app_tour_card.dart';
 part 'card_matkul_home.dart';
 part 'card_matkul_review.dart'; // Created by Muhamad Fauzi Ridwan on 07/11/21.
-part 'app_tour_card.dart';
+
+// ads
+part 'bikuntracker_ad.dart';
